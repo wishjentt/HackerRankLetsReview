e90063489919deb3052775ac67045f4d164b4a2c
wishjentt <wishjentt@github.com>
2019-06-01 13:07:37 +0300

    HackerRankDay6LetsReviewSolution

---
+++ HackerRankLetsReview/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankLetsReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            string[] s1 = new string[T];
+
+            int i = 0;
+            while (i < T)
+            {
+                string s = Console.ReadLine();
+                s1[i++] = s;
+            }
+
+            i = 0;
+            while (i < T)
+            {
+                char[] c1 = s1[i].ToCharArray();
+                string even = "";
+                string odd = "";
+                for (int j = 0; j < c1.Length; j++)
+                {
+                    if (j % 2 == 0) even += c1[j];
+                    if (j % 2 != 0) odd += c1[j];
+                }
+                Console.WriteLine(even + " " + odd);
+                i++;
+            }
+        }
+    }
+}
